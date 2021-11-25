7844bf37cf3767c4883a24364efaac1a8751de69
Udacity <support@udacity.com>
2021-11-25 08:35:00 -0800

    Add search field to ListContacts.

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,29 +1,48 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 const ListContacts = ({ contacts, onDeleteContact }) => {
+  const [query, setQuery] = useState("");
+
+  const updateQuery = (query) => {
+    setQuery(query.trim());
+  };
+
   return (
-    <ol className="contact-list">
-      {contacts.map((contact) => (
-        <li key={contact.id} className="contact-list-item">
-          <div
-            className="contact-avatar"
-            style={{
-              backgroundImage: `url(${contact.avatarURL})`,
-            }}
-          ></div>
-          <div className="contact-details">
-            <p>{contact.name}</p>
-            <p>{contact.handle}</p>
-          </div>
-          <button
-            className="contact-remove"
-            onClick={() => onDeleteContact(contact)}
-          >
-            Remove
-          </button>
-        </li>
-      ))}
-    </ol>
+    <div className="list-contacts">
+      {console.log(query)}
+      <div className="list-contacts-top">
+        <input
+          className="search-contacts"
+          type="text"
+          placeholder="Search Contacts"
+          value={query}
+          onChange={(event) => updateQuery(event.target.value)}
+        />
+      </div>
+      <ol className="contact-list">
+        {contacts.map((contact) => (
+          <li key={contact.id} className="contact-list-item">
+            <div
+              className="contact-avatar"
+              style={{
+                backgroundImage: `url(${contact.avatarURL})`,
+              }}
+            ></div>
+            <div className="contact-details">
+              <p>{contact.name}</p>
+              <p>{contact.handle}</p>
+            </div>
+            <button
+              onClick={() => onDeleteContact(contact)}
+              className="contact-remove"
+            >
+              Remove
+            </button>
+          </li>
+        ))}
+      </ol>
+    </div>
   );
 };
 
