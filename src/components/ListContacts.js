7bb2fccdfa549fd1317294ea22789b7a491b0d3f
Udacity <support@udacity.com>
2021-11-25 08:45:00 -0800

    Add 'Now showing' section with link to clear the search query.

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -8,6 +8,10 @@ const ListContacts = ({ contacts, onDeleteContact }) => {
     setQuery(query.trim());
   };
 
+  const clearQuery = () => {
+    updateQuery("");
+  };
+
   const showingContacts =
     query === ""
       ? contacts
@@ -26,6 +30,16 @@ const ListContacts = ({ contacts, onDeleteContact }) => {
           onChange={(event) => updateQuery(event.target.value)}
         />
       </div>
+
+      {showingContacts.length !== contacts.length && (
+        <div className="showing-contacts">
+          <span>
+            Now showing {showingContacts.length} of {contacts.length}
+          </span>
+          <button onClick={clearQuery}>Show all</button>
+        </div>
+      )}
+
       <ol className="contact-list">
         {showingContacts.map((contact) => (
           <li key={contact.id} className="contact-list-item">
