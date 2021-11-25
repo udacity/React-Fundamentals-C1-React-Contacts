b0add370cb3e234aca354823bf1900947bb2371d
Udacity <support@udacity.com>
2021-11-25 09:25:00 -0800

    Build form to create new contacts.

--- src/components/CreateContact.js
+++ src/components/CreateContact.js
@@ -1,5 +1,26 @@
+import { Link } from "react-router-dom";
+import ImageInput from "./ImageInput";
+
 const CreateContact = () => {
-  return <div>Create Contact</div>;
+  return (
+    <div>
+      <Link className="close-create-contact" to="/">
+        Close
+      </Link>
+      <form className="create-contact-form">
+        <ImageInput
+          className="create-contact-avatar-input"
+          name="avatarURL"
+          maxHeight={64}
+        />
+        <div className="create-contact-details">
+          <input type="text" name="name" placeholder="Name" />
+          <input type="text" name="handle" placeholder="Handle" />
+          <button>Add Contact</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CreateContact;
