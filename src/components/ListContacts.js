45b782007fdc03244639932eec61bac15fe24db6
Udacity <support@udacity.com>
2021-11-25 08:15:00 -0800

    Display contact avatar, name, and email. Create button for removal.

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -2,7 +2,19 @@ const ListContacts = ({ contacts }) => {
   return (
     <ol className="contact-list">
       {contacts.map((contact) => (
-        <li key={contact.id}>{contact.name}</li>
+        <li key={contact.id} className="contact-list-item">
+          <div
+            className="contact-avatar"
+            style={{
+              backgroundImage: `url(${contact.avatarURL})`,
+            }}
+          ></div>
+          <div className="contact-details">
+            <p>{contact.name}</p>
+            <p>{contact.handle}</p>
+          </div>
+          <button className="contact-remove">Remove</button>
+        </li>
       ))}
     </ol>
   );
