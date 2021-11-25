e6b2113e923393ab250630628e181e5b36d36943
Udacity <support@udacity.com>
2021-11-25 09:35:00 -0800

    Save new contact to the server.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import { useState, useEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import "../css/App.css";
 import ListContacts from "./ListContacts";
 import CreateContact from "./CreateContact.js";
 import * as ContactsAPI from "../utils/ContactsAPI";
 
 const App = () => {
+  let navigate = useNavigate();
+
   const [contacts, setContacts] = useState([]);
 
   useEffect(() => {
@@ -23,6 +25,16 @@ const App = () => {
     setContacts(contacts.filter((c) => c.id !== contact.id));
   };
 
+  const createContact = (contact) => {
+    const create = async () => {
+      const res = await ContactsAPI.create(contact);
+      setContacts(contacts.concat(res));
+    };
+
+    create();
+    navigate("/");
+  };
+
   return (
     <Routes>
       <Route
@@ -32,7 +44,16 @@ const App = () => {
           <ListContacts contacts={contacts} onDeleteContact={removeContact} />
         }
       />
-      <Route path="/create" element={<CreateContact />} />
+      <Route
+        path="/create"
+        element={
+          <CreateContact
+            onCreateContact={(contact) => {
+              createContact(contact);
+            }}
+          />
+        }
+      />
     </Routes>
   );
 };
