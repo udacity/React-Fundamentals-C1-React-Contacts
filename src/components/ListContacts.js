820f90f470285d481b56d230e572784c92937e53
Udacity <support@udacity.com>
2021-11-25 08:10:00 -0800

    Render default contacts.

--- src/components/ListContacts.js
+++ src/components/ListContacts.js
@@ -1,6 +1,11 @@
 const ListContacts = ({ contacts }) => {
-  console.log(contacts);
-  return <ol className="contact-list"></ol>;
+  return (
+    <ol className="contact-list">
+      {contacts.map((contact) => (
+        <li key={contact.id}>{contact.name}</li>
+      ))}
+    </ol>
+  );
 };
 
 export default ListContacts;
