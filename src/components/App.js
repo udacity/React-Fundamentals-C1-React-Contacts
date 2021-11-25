20ad016d713b3331bb0c64f40894094832ddbd1a
Udacity <support@udacity.com>
2021-11-25 08:05:00 -0800

    Add default contacts. Create the ListContacts component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,32 @@
 import "../css/App.css";
+import ListContacts from "./ListContacts";
 
 const App = () => {
-  return <div>Hello World</div>;
+  const contacts = [
+    {
+      id: "tyler",
+      name: "Tyler McGinnis",
+      handle: "@tylermcginnis",
+      avatarURL: "http://localhost:5001/tyler.jpg",
+    },
+    {
+      id: "karen",
+      name: "Karen Isgrigg",
+      handle: "@karen_isgrigg",
+      avatarURL: "http://localhost:5001/karen.jpg",
+    },
+    {
+      id: "richard",
+      name: "Richard Kalehoff",
+      handle: "@richardkalehoff",
+      avatarURL: "http://localhost:5001/richard.jpg",
+    },
+  ];
+  return (
+    <div>
+      <ListContacts contacts={contacts} />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/ListContacts.js
@@ -0,0 +1,6 @@
+const ListContacts = ({ contacts }) => {
+  console.log(contacts);
+  return <ol className="contact-list"></ol>;
+};
+
+export default ListContacts;
