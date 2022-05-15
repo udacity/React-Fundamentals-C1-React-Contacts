536c0904207202a4d4f1e77e9ef640862bf050a1
Pavel <p-olhovoi@bk.ru>
2022-05-15 11:51:38 +0200

    add the Contacts component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,32 @@
 import "../css/App.css";
+// import logo from "../../public/logo192.png";
+import Components from "../components/Contacts";
 
 const App = () => {
-  return <div>Hello World</div>;
+  const contacts = [
+    {
+      id: "karen",
+      name: "Karen Isgrigg",
+      handle: "karen_isgrigg",
+      avatarURL: "http://localhost:5001/karen.jpg",
+    },
+    {
+      id: "richard",
+      name: "Richard Kalehoff",
+      handle: "richardkalehoff",
+      avatarURL: "http://localhost:5001/richard.jpg",
+    },
+    {
+      id: "tyler",
+      name: "Tyler McGinnis",
+      handle: "tylermcginnis",
+      avatarURL: "http://localhost:5001/tyler.jpg",
+    },
+  ];
+
+  return <div>
+        <Components contacts={contacts} />
+    </div>;
 };
 
 export default App;
---
+++ src/components/Contacts.js
@@ -0,0 +1,22 @@
+
+let Contacts = ({ contacts }) => {
+    console.log(contacts);
+    return (
+        <ol className="contact-list">
+        {
+            contacts.map( contact => (
+                <li key={contact.id} className="contact-list-item">
+                    <div className="contact-avatar" style={{backgroundImage: `url(${contact.avatarURL})`}}></div>
+                    <div className="contact-details">
+                        <p>{contact.name}</p>
+                        <p>{contact.handle}</p>
+                    </div>
+                    <button className="contact-remove">Remove</button>
+                </li>
+            ))
+        }
+    </ol>
+    )
+}
+
+export default Contacts;
